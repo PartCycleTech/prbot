a110b7430310b25f8f43a78f8eb4493abe7a5440
Patrick Lindsay <patrick@partcycle.com>
2017-05-16 16:31:48 -0500

    Initial commit

---
+++ prbot/lib/prbot.ex
@@ -0,0 +1,18 @@
+defmodule Prbot do
+  @moduledoc """
+  Documentation for Prbot.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Prbot.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ prbot/test/prbot_test.exs
@@ -0,0 +1,8 @@
+defmodule PrbotTest do
+  use ExUnit.Case
+  doctest Prbot
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
