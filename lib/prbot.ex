90be6fbe08a7ee1bb05219187d38c7573e323863
Patrick Lindsay <patrick@partcycle.com>
2017-05-17 15:11:07 -0500

    Write some Slack boilerplate

--- lib/prbot.ex
+++ lib/prbot.ex
@@ -2,6 +2,11 @@ defmodule Prbot do
   use Application
 
   def start(_type, _args) do
+    unless Mix.env == :prod do
+      Envy.auto_load
+    end
+
+    IO.puts System.get_env("SLACK_TOKEN")
     IO.puts "running"
     children = [ Plug.Adapters.Cowboy.child_spec(:http, Prbot.Router, [], port: 4000) ]
     Supervisor.start_link(children, strategy: :one_for_one)
