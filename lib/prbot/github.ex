6da120dd6d71cd1216140ee3b433794dbba35b0a
Tyler Yasaka <contact@tyleryasaka.me>
2017-05-18 02:29:40 -0500

    Defines some methods for working with github.

--- lib/prbot/github.ex
+++ lib/prbot/github.ex
@@ -7,4 +7,32 @@ defmodule Prbot.Github do
     Tentacat.Pulls.list "PartCycleTech", "partcycle-backend", client
   end
 
+  def getPRNumbers do
+    for pr <- getPRs, do: pr["number"]
+  end
+
+  def getPRRefs do
+    for pr <- getPRs, do: pr["head"]["ref"]
+  end
+
+  def getReviews(prNumber) do
+    Tentacat.Pulls.Reviews.list "PartCycleTech", "partcycle-backend", prNumber, client
+  end
+
+  def getCombinedStatuses(ref) do
+    response = Tentacat.Repositories.Statuses.find "PartCycleTech", "partcycle-backend", ref, client
+    response["statuses"]
+  end
+
+  def getReviewsForAllPRs do
+    for prNumber <- getPRNumbers, do: getReviews(prNumber)
+  end
+
+  def getCombinedStatusesForAllPRs do
+    for ref <- getPRRefs, do: getCombinedStatuses(ref)
+  end
+
+  def getReviewsStates(reviews) do
+    for review <- reviews, do: review["state"]
+  end
 end
